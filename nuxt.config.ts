1c2e3bf3dd6e24d98042dbb00a7ad996722d7268
Vlad <tyweapon@gmail.com>
2024-05-24 21:50:21 -0300

    added ant design system

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/styles/main.scss'],
   modules: ['nuxt-svgo'],
   vite: {
+    plugins: [
+      Components({
+        // add option {resolveIcons: true} as parameter for resolving problem with icons
+        resolvers: [AntDesignVueResolver({resolveIcons: true})],
+      }),
+    ],
+    // @ts-expect-error: Missing ssr key
+    ssr: {
+      noExternal: ['moment', 'compute-scroll-into-view', 'ant-design-vue','@ant-design/icons-vue'],
+    },
     css: {
         preprocessorOptions: {
             scss: {
